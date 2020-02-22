054faf8ea305f09a135d0ba077f4a4cbf04e41d4
ejilee <whyejile@gmail.com>
2020-02-22 17:26:32 +0900

    fixed tocscrollspy.js and blog entry about it

--- scaffolds/post.md
+++ scaffolds/post.md
@@ -1,7 +1,7 @@
 ---
 title: {{ title }}
 date: {{ date }}
-author: "Yeji"
+author: "Weiji"
 tags: []
 categories:
   - [Uncategorized]
--- themes/weiji/source/js/tocscrollspy.js
+++ themes/weiji/source/js/tocscrollspy.js
@@ -2,26 +2,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	const articleBody = document.getElementsByClassName("article-entry")[0];	
 	const articleToc = document.getElementById("article-toc");
-	const allHeadings = articleToc.querySelectorAll("li.toc-item");
+	const allTocLis = articleToc.querySelectorAll("li.toc-item");
 
 	const observer = new IntersectionObserver(entries => {
 		entries.forEach(entry => {
-			const headerTitle = entry.target.getAttribute('title');
-			const headerTitleParsed = headerTitle.replace(/[`~!@#$%^*()_|+\=?;:,.\{\}\[\]\\\/]/gi, '').replace("&",'amp').replace("<",'-lt').replace(">",'-gt').replace(/\s+/g, '-');
 
+			const headerTitle = entry.target.getAttribute('title');
+			const headerTitleFixed = headerTitle.replace(/[`~!@#$%^*()_|+\=?;:,.\{\}\[\]\\\/]/gi, '').replace("&",'amp').replace("<",'-lt').replace(">",'-gt').replace(/\s+/g, '-').replace('---','-');
+			
 			if (entry.intersectionRatio > 0) {
-				[].forEach.call(allHeadings, function(el) {
-					el.classList.remove("active");
-				});
-				console.log(headerTitleParsed);
-				const linkSpan = articleToc.querySelector(`li a[href="#${headerTitleParsed}"]`);
-				if(linkSpan) linkSpan.closest('li.toc-level-1').classList.add('active');
+				allTocLis.forEach(el => { el.classList.remove("active"); });
+				articleToc.querySelector(`li a[href="#${headerTitleFixed}"]`).parentElement.classList.add('active');
 			}
 
 		});
-	});
+	},{rootMargin:'-60px'});
 
-    // Track all sections that have an `id` applied
 	articleBody.querySelectorAll('.headerlink').forEach((headerlink) => {
 		observer.observe(headerlink);
 	});
