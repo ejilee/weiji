3d59f56949cf67a4892b67f7a83f93dc6957cfc8
ejilee <whyejilee@gmail.com>
2020-03-02 15:27:10 +0900

    2020-03-02 15:27:09

---
+++ js/tocscrollspy.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+	const articleBody = document.getElementsByClassName("article-entry")[0];	
+	const articleToc = document.getElementById("article-toc");
+	const allTocLis = articleToc.querySelectorAll("li.toc-item");
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+
+			const headerTitle = entry.target.getAttribute('title');
+			const headerTitleFixed = headerTitle.replace(/[`~!@#$%^*()_|+\=?;:,.\{\}\[\]\\\/]/gi, '').replace("&",'amp').replace("<",'-lt').replace(">",'-gt').replace(/\s+/g, '-').replace('---','-');
+			
+			if (entry.intersectionRatio > 0) {
+				allTocLis.forEach(el => { el.classList.remove("active"); });
+				articleToc.querySelector(`li a[href="#${headerTitleFixed}"]`).parentElement.classList.add('active');
+			}
+
+		});
+	},{rootMargin:'-60px'});
+
+	articleBody.querySelectorAll('.headerlink').forEach((headerlink) => {
+		observer.observe(headerlink);
+	});
+	
+});
