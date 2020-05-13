0afa11c82179deb82d399ec5f50ad9f72cdaac2f
ejilee <whyejilee@gmail.com>
2020-05-13 19:48:30 +0900

    2020-05-13 19:48:29

--- js/tocscrollspy.js
+++ js/tocscrollspy.js
@@ -1,25 +1,37 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  const articleBody = document.getElementsByClassName("article-entry")[0];
+  const articleToc = document.getElementById("article-toc");
+  const allTocLis = articleToc.querySelectorAll("li.toc-item");
 
-	const articleBody = document.getElementsByClassName("article-entry")[0];	
-	const articleToc = document.getElementById("article-toc");
-	const allTocLis = articleToc.querySelectorAll("li.toc-item");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const headerTitle = entry.target.getAttribute("title");
+        const headerTitleFixed = headerTitle
+          .replace(/[`~!@#$%^*()_|+\=?;:,\{\}\[\]\\\/]/gi, "")
+          .replace(/&/g, "amp")
+          .replace(/</g, "lt")
+          .replace(/>/g, "gt")
+          .replace(/\s+/g, "-")
+          .replace(/\./g, "-")
+          .replace(/---/g, "-");
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
+        console.log(headerTitleFixed);
 
-			const headerTitle = entry.target.getAttribute('title');
-			const headerTitleFixed = headerTitle.replace(/[`~!@#$%^*()_|+\=?;:,.\{\}\[\]\\\/]/gi, '').replace("&",'amp').replace("<",'-lt').replace(">",'-gt').replace(/\s+/g, '-').replace('---','-');
-			
-			if (entry.intersectionRatio > 0) {
-				allTocLis.forEach(el => { el.classList.remove("active"); });
-				articleToc.querySelector(`li a[href="#${headerTitleFixed}"]`).parentElement.classList.add('active');
-			}
+        if (entry.intersectionRatio > 0) {
+          allTocLis.forEach((el) => {
+            el.classList.remove("active");
+          });
+          articleToc
+            .querySelector(`li a[href="#${headerTitleFixed}"]`)
+            .parentElement.classList.add("active");
+        }
+      });
+    },
+    { rootMargin: "-60px" }
+  );
 
-		});
-	},{rootMargin:'-60px'});
-
-	articleBody.querySelectorAll('.headerlink').forEach((headerlink) => {
-		observer.observe(headerlink);
-	});
-	
+  articleBody.querySelectorAll(".headerlink").forEach((headerlink) => {
+    observer.observe(headerlink);
+  });
 });
